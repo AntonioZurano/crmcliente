2a3119141d8f3fb037f2b8a5404c1a25efd7996a
azuranob <azurano@outlook.es>
2023-07-28 23:36:52 +0200

    add component Layout

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Layout from '../components/Layout';
+
+const Index = () =>( 
+    <div>
+        <Layout>
+            <h1>Desde Home</h1>
+        </Layout>
+    </div>
+) 
+
+export default Index;
