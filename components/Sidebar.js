f2ef38b671fa1888f1efc42ef3c0b09f9907d6d0
azuranob <azurano@outlook.es>
2023-07-29 01:15:48 +0200

    Modify Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Sidebar = () => {
+
+    // Routing de next
+    const router = useRouter();
+
+    //console.log(router.pathname);
+
     return (
         <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
             <div>
                 <p className="text-white text-2xl font-black">CRM Clientes</p>
             </div>
             <nav className="mt-5 list-none">
-                <li>
+                <li className={router.pathname === "/" ? "bg-blue-800 p-3" : "p-3"}>
                     <Link href="/">
                         <p className="text-white mb-2 block">
                             Clientes
                         </p>
                     </Link>
                 </li>
-                <li>
+                <li className={router.pathname === "/pedidos" ? "bg-blue-800 p-3" : "p-3"}>
                     <Link href="/pedidos">
                         <p className="text-white mb-2 block">
                             Pedidos
+                        </p>
+                    </Link>
+                </li>
+                <li className={router.pathname === "/productos" ? "bg-blue-800 p-3" : "p-3"}>
+                    <Link href="/productos">
+                        <p className="text-white mb-2 block">
+                            Productos
                         </p>
                     </Link>
                 </li>
