b613b8b046ee8b2471e003eff430ee2aa1307281
azuranob <azurano@outlook.es>
2023-07-31 13:54:41 +0200

    create form nuevacuenta

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -2,13 +2,77 @@ import React from 'react';
 import Layout from '../components/Layout';
 
 const NuevaCuenta = () => {
-    return ( 
+    return (
+
         <>
+
             <Layout>
-                <h1 className="text-center text-2xl text-white font-light">Nueva Cuenta</h1>
+                <h1 className="text-center text-2xl text-white font-light">Crear Nueva Cuenta</h1>
+
+                <div className="flex justify-center mt-5">
+                    <div className="w-full max-w-sm">
+                        <form
+                            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                        >
+                            <div className="mb-4">
+                                <label
+                                    className="block text-gray-700 text-sm font-bold mb-2"
+                                    htmlFor="nombre"
+                                >Nombre</label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="nombre"
+                                    type="text"
+                                    placeholder="Nombre Usuario"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label
+                                    className="block text-gray-700 text-sm font-bold mb-2"
+                                    htmlFor="apellido"
+                                >Nombre</label>
+                                <input
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="apellido"
+                                    type="text"
+                                    placeholder="Apellidos Usuario"
+                                />
+                            </div>                            
+                            <div className="mb-4">
+                                <label
+                                    className="block text-gray-700 text-sm font-bold mb-2"
+                                    htmlFor="email"
+                                >Email</label>
+                                <input
+                                    type="email"
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="email"
+                                    placeholder="Email Usuario"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label
+                                    className="block text-gray-700 text-sm font-bold mb-2"
+                                    htmlFor="password"
+                                >Password</label>
+                                <input
+                                    type="password"
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    id="password"
+                                    placeholder="Password Usuario"
+                                />
+                            </div>
+                            <input
+                                type="submit"
+                                className="bg-gray-800 w-full mt-5 p-2 text-white hover:bg-gray-900"
+                                value="Crear Cuenta"
+                            />
+                        </form>
+                    </div>
+                </div>
             </Layout>
         </>
-     );
+    );
 }
 
 export default NuevaCuenta;
