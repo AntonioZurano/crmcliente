ebe3a6036a6d9a4d69f794baf8c7cc59a6935a32
azuranob <azurano@outlook.es>
2023-08-05 00:22:27 +0200

    Add formik to project in nuevacuenta

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const NuevaCuenta = () => {
+
+
+    // Formulario y validación con formik y Yup
+    const formik = useFormik({
+        initialValues: {
+            nombre: '',
+            apellido: '',
+            email: '',
+            password: ''
+        },
+        validationSchema: Yup.object({
+            nombre: Yup.string()
+                .required('El nombre es obligatorio'),
+            apellido: Yup.string()
+                .required('El apellido es obligatorio'),
+            email: Yup.string()
+                .email('El email no es válido')
+                .required('El email es obligatorio'),
+            password: Yup.string()
+                .required('El password no puede ir vacío')
+                .min(6, 'El password debe ser de al menos 6 caracteres')
+        }),
+        onSubmit: valores => {
+            console.log(valores);
+        }
+    });
+
     return (
 
         <>
@@ -13,6 +42,8 @@ const NuevaCuenta = () => {
                     <div className="w-full max-w-sm">
                         <form
                             className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                            onSubmit={formik.handleSubmit}
+                            
                         >
                             <div className="mb-4">
                                 <label
@@ -20,10 +51,12 @@ const NuevaCuenta = () => {
                                     htmlFor="nombre"
                                 >Nombre</label>
                                 <input
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700  leading-tight focus:outline-none focus:shadow-outline"
                                     id="nombre"
                                     type="text"
                                     placeholder="Nombre Usuario"
+                                    value={formik.values.nombre}
+                                    onChange={formik.handleChange}
                                 />
                             </div>
                             <div className="mb-4">
@@ -36,6 +69,8 @@ const NuevaCuenta = () => {
                                     id="apellido"
                                     type="text"
                                     placeholder="Apellidos Usuario"
+                                    value={formik.values.apellido}
+                                    onChange={formik.handleChange}
                                 />
                             </div>                            
                             <div className="mb-4">
@@ -48,6 +83,8 @@ const NuevaCuenta = () => {
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="email"
                                     placeholder="Email Usuario"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
                                 />
                             </div>
                             <div className="mb-4">
@@ -60,6 +97,8 @@ const NuevaCuenta = () => {
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="password"
                                     placeholder="Password Usuario"
+                                    value={formik.values.password}
+                                    onChange={formik.handleChange}
                                 />
                             </div>
                             <input
